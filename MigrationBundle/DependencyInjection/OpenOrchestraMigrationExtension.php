c287df1f7b0c7295b295954fd5848cab93054784
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-12-12 14:06:20 +0100

    init repo migration mongo

---
+++ MigrationBundle/DependencyInjection/OpenOrchestraMigrationExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OpenOrchestra\MigrationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
+class OpenOrchestraMigrationExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+    }
+}
---
+++ MigrationBundle/OpenOrchestraMigrationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OpenOrchestra\MigrationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OpenOrchestraMigrationBundle extends Bundle
+{
+}
